378da53fcc272a3b8a1cdceffe285f0370d6a1c7
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-11-07 19:00:01 +0600

    Add screen for show number and add 7,8,9,/ button

--- app/src/main/java/com/yhr/jfj/calculator_xlm/MainActivity.kt
+++ app/src/main/java/com/yhr/jfj/calculator_xlm/MainActivity.kt
@@ -2,10 +2,17 @@ package com.yhr.jfj.calculator_xlm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // Show digit
+    fun onDigit(view: View){
+        Toast.makeText(this, "Button clicked", Toast.LENGTH_LONG).show()
     }
 }
