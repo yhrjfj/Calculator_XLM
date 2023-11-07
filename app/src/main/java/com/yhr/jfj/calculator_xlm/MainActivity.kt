a796fbd7683b5949d044f84126c0c0b5f3c077c2
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-11-07 20:43:57 +0600

    Show dot when dot button has been clicked

--- app/src/main/java/com/yhr/jfj/calculator_xlm/MainActivity.kt
+++ app/src/main/java/com/yhr/jfj/calculator_xlm/MainActivity.kt
@@ -10,6 +10,11 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
 
     private var tvInput: TextView ?= null
+
+    // Use this for checking if user use dot to a previous number
+    private var lastNumeric: Boolean = false
+    private var lastDot: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,11 +25,22 @@ class MainActivity : AppCompatActivity() {
     // Show digit
     fun onDigit(view: View){
         tvInput?.append((view as Button).text)
+        lastNumeric = true
+        lastDot = false
     }
 
     // Clear screen
     fun onClear(view: View){
         tvInput?.text = ""
+    }
+
+    // Decimal point
+    fun onDecimalPoint(view: View){
+        if (lastNumeric && !lastDot){
+            tvInput?.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
     }
 
 }
